c1563c7494e8ac6e72b1fe19996e39d275098601
John Hobbs <john@velvetcache.org>
2020-12-08 23:29:48 -0600

    Problem 5B

--- 05/main.js
+++ 05/main.js
@@ -16,8 +16,26 @@ function loadInput(cb) {
 
 loadInput((lines) => {
   let highest = 0;
+  const found = [];
   lines.forEach(line => {
-    highest = Math.max(highest, a.seatID(...a.findSeat(line)));
+    const id = a.seatID(...a.findSeat(line));
+    found.push(id);
+    highest = Math.max(highest, id);
   });
   console.log('A:', highest);
+  
+  let last = 0;
+
+  found.map((v) => parseInt(v, 10)).sort((l, r) => l - r).forEach((id) => {
+    if(last === 0) {
+      last = id;
+      return;
+    }
+
+    if(id - last > 1) {
+      console.log('B:', id - 1);
+    }
+
+    last = id;
+   });
 });
